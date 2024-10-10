bd17f4d4d68412adc25006e30b1e2774ce172252
wwwpeps <amongas8903@gmail.com>
2024-10-10 13:21:19 +0500

    Добавьте файлы проекта.

---
+++ meth/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meth
+{
+    public class math 
+    {
+        public static double Circlesr(double r)
+        {
+            double s = 0;
+            return s = 3.14 * (r * r);
+        }
+
+        public static double Circlesp(double r)
+        {
+            double p = 0;
+            return p = (2 * 3.14) * r * r;
+        }
+
+        public static double Squarer(double a)
+        {
+            double s = 0;
+            return s = a * a;
+        }
+
+        public static double Squarep(double a)
+        {
+            double p = 0;
+            return p = 4 * a;
+        }
+    }
+}
---
+++ pr10.3/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using meth;
+
+namespace pr10._3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double r = 0;
+        double a = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ba_Click(object sender, RoutedEventArgs e)
+        {
+            ta.Clear();
+            tb.Clear();
+            a = Convert.ToDouble(td.Text);
+            r = Convert.ToDouble(tc.Text);
+            ta.Text += "Square: " + meth.math.Circlesr(r) + " ;" + Environment.NewLine + "perimeter: " + meth.math.Circlesr(r) + " .";
+            tb.Text += "Square: " + meth.math.Squarer(a) + " ;" + Environment.NewLine + "perimeter: " + meth.math.Squarep(a) + " .";
+        }
+    }
+}
